f9a6476e28c0262a34e923f72b440f11a59e79b6
Samantha-fu <847039518@qq.com>
2019-07-29 13:42:54 +0800

    Update get_sepsis_score.py

--- get_sepsis_score.py
+++ get_sepsis_score.py
@@ -15,9 +15,10 @@ def get_sepsis_score(data,model):#current
     if add_data.shape[1]==170:
         score =model.predict_proba(add_data)[:,1]
         label = score > 0.4
+    return score,label
     else:
         print("the shape is not match 170 ,check")
-    return score,label
+    return None
 def load_sepsis_model():
     model =joblib.load("./train56000_model.m")
     return model
